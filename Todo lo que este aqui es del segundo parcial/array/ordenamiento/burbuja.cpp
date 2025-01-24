d36ff331367f22351c96df4f62ca1b0256dc98d9
luis bocancho <luisbocancho7@.gmail.com>
2025-01-24 10:53:32 -0500

    Prueba_Parcial2

--- Todo lo que este aqui es del segundo parcial/array/ordenamiento/burbuja.cpp
+++ Todo lo que este aqui es del segundo parcial/array/ordenamiento/burbuja.cpp
@@ -1,46 +1,127 @@
+//https://github.com/Luisitobc/LuisBC_1946
 #include <iostream>
 #include <stdio.h>
 #include <time.h>
 using namespace std;
 
-void llenaVector (int v[],int n){
+void llenaVector(int v[], int n) {
     srand(time(NULL));
-    for(int i=0; i<n;i++){
-        v[i]=rand()%20+1;
+    for (int i=0; i<n; i++) {
+        cin>>v[i];
     }
 }
-void ordenarBurbunja(int v[], int n){
 
-    int aux;
-    for (int i=0; i<n;i++){
-        for(int j=0;j<n-1;j++){
-            if (v[j]>v[j+1]){
-                aux=v[j];
+void copiarVector(int origen[], int destino[], int n) {
+    for(int i = 0; i < n; i++) {
+        destino[i] = origen[i];
+    }
+}
+
+int ordenarBurbujaV1(int v[], int n) {
+    int aux,contador=0;
+    for (int i=0;i<n;i++) {
+        for (int j=0; j<n-1; j++) {
+            contador++;
+            if (v[j]>v[j+1]) {
+                aux =v[j];
                 v[j]=v[j+1];
                 v[j+1]=aux;
             }
         }
     }
+    return contador;
 }
-void muestraVector(int v[],int n){
-    for(int i=0;i<n;i++){
-        cout<<v{i}<<" ";
+
+int ordenarBurbujaV2(int v[], int n) {
+    int mejora=1;
+    int aux,contador=0;
+    for (int i=0;i<n;i++) {
+        for (int j=0; j<n-mejora; j++) {
+            contador++;
+            if (v[j]>v[j+1]) {
+                aux =v[j];
+                v[j]=v[j+1];
+                v[j+1]=aux;
+            }
+        }
+        mejora++;
     }
-
+    return contador;
 }
 
-main(){
+int ordenarBurbujaV3(int v[], int n) {
+    int mejora=1;
+    bool cambio=true;
+    int aux,contador=0;
+    for (int i=0;i<n && cambio;i++) {
+        cambio=false;
+        for (int j=0; j<n-mejora; j++) {
+            contador++;
+            if (v[j]>v[j+1]) {
+                aux =v[j];
+                v[j]=v[j+1];
+                v[j+1]=aux;
+                cambio=true;
+            }
+        }
+        mejora++;
+    }
+    return contador;
+}
 
+void muestraVector(int v[], int n) {
+    for (int i=0; i<n; i++) {
+        cout<<v[i]<<" ";
+    }
+}
+
+void escogerMejorBurbuja(int v[], int n) {
+    int v1[n], v2[n], v3[n];
+    
+
+    copiarVector(v, v1, n);
+    copiarVector(v, v2, n);
+    copiarVector(v, v3, n);
+    
+
+    int comp1 = ordenarBurbujaV1(v1, n);
+    int comp2 = ordenarBurbujaV2(v2, n);
+    int comp3 = ordenarBurbujaV3(v3, n);
+    
+    cout << "\nComparaciones realizadas por cada version:";
+    cout << "\nBurbuja V1: " << comp1;
+    cout << "\nBurbuja V2: " << comp2;
+    cout << "\nBurbuja V3: " << comp3;
+    
+
+    if(comp1 <= comp2 && comp1 <= comp3) {
+        cout << "\nLa Burbuja V1 es la mas eficiente";
+        copiarVector(v1, v, n);
+    }
+    else if(comp2 <= comp1 && comp2 <= comp3) {
+        cout << "\nLa Burbuja V2 es la mas eficiente";
+        copiarVector(v2, v, n);
+    }
+    else {
+        cout << "\nLa Burbuja V3 es la mas eficiente";
+        copiarVector(v3, v, n);
+    }
+}
+
+main() {
     int ne;
-    cout <<" Numero de elementos ";
-    cin>> ne ;
+    cout<<"Numeros de elementos: ";
+    cin>>ne;
     int vec[ne];
-    llenaVector (vec,ne);
-    cout << " Vector original ";
+    llenaVector(vec,ne);
+    cout<<"Vector original: ";
     muestraVector(vec,ne);
-    cout <<endl;
-    ordenarBurbunja(vec,ne);
-    cout<<" Vector ordenado ";
+    cout<<endl;
+    
+    escogerMejorBurbuja(vec,ne);
+    
+    cout<<"\nVector ordenado con la mejor version: ";
     muestraVector(vec,ne);
-    cout<< " Vector "
+    cout<<endl;
+    
 }
